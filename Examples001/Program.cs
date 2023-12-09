c80ca5bcfa9d260dcdf30ab5983adfa4d74e4282
Maks Fedyunev <cmaksf@mail.ru>
2023-12-09 16:45:56 +0300

    first

---
+++ Examples001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+{
+    Console.WriteLine("Введите значение M:");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите значение N:");
+    int n = int.Parse(Console.ReadLine());
+
+    PrintNumbersInRange(m, n);
+
+    Console.ReadLine();
+}
+
+static void PrintNumbersInRange(int start, int end)
+{
+    if (start <= end)
+    {
+        Console.Write(start + " ");
+        PrintNumbersInRange(start + 1, end);
+    }
+}
+
